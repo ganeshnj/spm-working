4469fd2b09d3d74f07b91defde126db6810780ad
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2024-05-15 13:48:34 +0200

    force change to false commit

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SPMWorking"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Datadog/dd-sdk-ios.git", branch: "dogfooding")
+        .package(url: "https://github.com/Datadog/dd-sdk-ios.git", branch: "develop")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
